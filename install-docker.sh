c7bd1fd358c4d0df85ddd60d06abd09b3370ecb2
root <root@ip-172-26-5-161.ec2.internal>
2023-08-11 14:14:49 +0000

    project

---
+++ install-docker.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# description:install docker in ubuntu 20.04
+# author:hercelle
+# date :08/11/23
+
+sudo apt install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+apt-cache policy docker-ce
+sudo apt install docker-ce
+sudo systemctl status docker
+
+
